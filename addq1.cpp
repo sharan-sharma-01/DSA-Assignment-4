c9f5a3ce75d02ab4eaa7a872fdd53e70b11c8abb
Sharan Sharma <sharan22042006@gmail.com>
2025-11-19 01:44:10 +0530

    Create addq1.cpp

---
+++ addq1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void generateBinary(int n) {
+    queue<string> q;
+    q.push("1");
+
+    for (int i = 0; i < n; i++) {
+        string curr = q.front();
+        q.pop();
+
+        cout << curr << " ";
+
+        q.push(curr + "0");
+        q.push(curr + "1");
+    }
+}
+
+int main() {
+    int n = 2;
+    generateBinary(n);
+    return 0;
+}
